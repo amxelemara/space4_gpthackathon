5c843a5c11f64bbe2b91cb70483b1cb71d1f8fba
Am <amxelemara@gmail.com>
2023-03-10 22:03:06 +0000

    Move lambda handler functions to own files

--- handler.ts
+++ handler.ts
@@ -1,55 +1,9 @@
 import "source-map-support/register";
 import { Context, APIGatewayEvent, APIGatewayProxyResultV2 } from "aws-lambda";
-// import axios from 'axios';
 
-export const serve = async (event: APIGatewayEvent, _context: Context): Promise<APIGatewayProxyResultV2> => {
-  try {
-    // We use asynchronous import here so we can better catch server-side errors during development
-    const render = (await import("./src/server/render")).default;
-    return {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "text/html",
-      },
-      body: await render(event),
-    };
-  } catch (error) {
-    // Custom error handling for server-side errors
-    // TODO: Prettify the output, include the callstack, e.g. by using `youch` to generate beautiful error pages
-    console.error(error);
-    return {
-      statusCode: 500,
-      headers: {
-        "Content-Type": "text/html",
-      },
-      body: `<html><body>${error.toString()}</body></html>`,
-    };
-  }
-};
+import { testHandler } from "./handlers/test"
+import { serveHandler } from "./handlers/serve"
 
-export const test = async (event: APIGatewayEvent, _context: Context): Promise<APIGatewayProxyResultV2> => {
-  try {
-    // Make API call
-    // const response = await axios.get('https://example.com/api/data');
 
-    // Return response as APIGatewayProxyResult
-    return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({message: 'success'}),
-    };
-  } catch (error) {
-    console.error(error);
-    // Return error as APIGatewayProxyResult
-    return {
-      statusCode: 500,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ message: 'Internal Server Error' }),
-    };
-  }
-};
-
+export const test = testHandler
+export const serve = serveHandler
---
+++ handlers/test.ts
@@ -0,0 +1,30 @@
+import "source-map-support/register";
+import { Context, APIGatewayEvent, APIGatewayProxyResultV2 } from "aws-lambda";
+// import axios from 'axios';
+
+export const testHandler = async (event: APIGatewayEvent, _context: Context): Promise<APIGatewayProxyResultV2> => {
+  try {
+    // Make API call
+    // const response = await axios.get('https://example.com/api/data');
+
+    // Return response as APIGatewayProxyResult
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({message: 'success'}),
+    };
+  } catch (error) {
+    console.error(error);
+    // Return error as APIGatewayProxyResult
+    return {
+      statusCode: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ message: 'Internal Server Error' }),
+    };
+  }
+};
+
