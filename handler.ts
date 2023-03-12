88f9e35aa796394d7e21b7a0c4e95e68f2c33a31
Am <amxelemara@gmail.com>
2023-03-12 17:48:27 +0000

    Endpoint to fill database with dummy data

--- handler.ts
+++ handler.ts
@@ -5,9 +5,11 @@ import { testHandler } from "./handlers/test"
 import { serveHandler } from "./handlers/serve"
 import { chatHandler } from "handlers/chatgpt";
 import { articleHandler } from "handlers/article"
+import { testDataHandler } from "handlers/test_data"
 
 
 export const chat = chatHandler
 export const article = articleHandler
 export const test = testHandler
 export const serve = serveHandler
+export const testData = testDataHandler
