3de565dfeb23c5e3e7539d38d9ad79a1c68e0539
Ali Clark <ali@aliclark.net>
2023-03-12 20:12:18 +0000

    Unsuccusful attempt getting IAM working, but try Access Credential next

--- handlers/summarizerpy.py
+++ handlers/summarizerpy.py
@@ -1,3 +1,16 @@
+
+import site
+print(site.getsitepackages())
+
+import sys
+
+# Get the site-packages directory path
+site_packages_dir = site.getsitepackages()[0]
+
+# Add the site-packages directory to the sys.path list
+sys.path.append(site_packages_dir)
+
+import os
 import json
 
 import boto3
@@ -7,17 +20,23 @@ import openai
 def handler(event, context):
 
     # Set up OpenAI API credentials
-    openai.api_key = process.env.OPENAI_API_KEY
+    openai.api_key = os.environ.get('OPENAI_API_KEY')
 
     encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
 
-    s3 = boto3.client('s3')
+    s3 = boto3.client('s3', region_name='eu-west-1')
     for record in event['Records']:
         bucket = record['s3']['bucket']['name']
         key = record['s3']['object']['key']
-        print(f"Received object with key: {key}")
+        print(f"Received object with bucket: {bucket}, key: {key}")
 
+
+        # FIXME: Permissions error in Serverless Framework..!?
+
+        # TODO: Try to use an AWS Access Credential instead of IAM Role
         obj = s3.get_object(Bucket=bucket, Key=key)
+
+
         body = obj['Body'].read()
 
         markdown = '<p>This is the markdown</p>'
@@ -102,6 +121,19 @@ def handler(event, context):
         print(sum_start_and_end_only_5y['choices'][0]['message']['content'])
         print()
 
+        # use boto3 to update dynamodb table with the summary:
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table(os.environ.get('SUMMARIZER_TABLE_NAME'))
+        table.update_item(
+            Key={
+                'id': key
+            },
+            UpdateExpression="set summary = :s",
+            ExpressionAttributeValues={
+                ':s': sum_start_and_end_only_5y['choices'][0]['message']['content']
+            },
+            ReturnValues="UPDATED_NEW"
+        )
 
 
 
